52354b8bfd4d905ea3dad18990aa4b3aa657ff7c
Matt Neal <neal.matt.m@gmail.com>
2022-07-28 17:34:28 -0400

    refactoring

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const inquirer = require('inquirer')
-const Manager  = require('./lib/manager.js')
-const Engineer = require('./lib/engineer.js')
-const Intern   = require('./lib/intern.js')
+const fs               = require('fs')
+const inquirer         = require('inquirer')
+const Manager          = require('./lib/manager.js')
+const Engineer         = require('./lib/engineer.js')
+const Intern           = require('./lib/intern.js')
+const renderTeamRoster = require('./src/roster.js')
 
 const managerQuestions = [{type: 'input',  name: 'name',   message: "Enter team manager's name"}
                          ,{type: 'number', name: 'id',     message: "Enter team manager's employee ID"}
@@ -20,32 +22,32 @@ const internQuestions = [{type: 'input',  name: 'name',   message: "Enter intern
 
 const menuOptions = ['add an engineer to my team', 'add an intern to my team', 'finish building my team']
 
+var team = []
+
 function presentMenu() {
     inquirer.prompt([{type: 'list', name: 'option', message: 'Choose an option', choices: menuOptions}])
     .then(choice => {
         switch (choice.option) {
             case menuOptions[0]:
                 inquirer.prompt(engineerQuestions).then(answers => {
-                    employees.push(new Engineer(answers.name, answers.id, answers.email, answers.github))
+                    team.push(new Engineer(answers.name, answers.id, answers.email, answers.github))
                     presentMenu()
                 })
                 break
             case menuOptions[1]:
                 inquirer.prompt(internQuestions).then(answers => {
-                    employees.push(new Intern(answers.name, answers.id, answers.email, answers.school))
+                    team.push(new Intern(answers.name, answers.id, answers.email, answers.school))
                     presentMenu()
                 })
                 break
             case menuOptions[2]:
-                console.log(employees)
+                fs.writeFile('./dist/roster.html', renderTeamRoster(team), err => { if(err) throw err })
                 break
         }
     })
 }
 
-var employees = []
-
 inquirer.prompt(managerQuestions).then(answers => {
-    employees.push(new Manager(answers.name, answers.id, answers.email, answers.office))
+    team.push(new Manager(answers.name, answers.id, answers.email, answers.office))
     presentMenu()
 })
---
+++ src/roster.js
@@ -0,0 +1,47 @@
+function renderTeamRoster(team) {
+    var employees = ''
+
+    team.forEach(x => {employees += renderEmployee(x)})
+
+    return `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Team Roster</title>
+            </head>
+            <body>
+                <h1>My Team</h1>
+                <ul>
+                ${employees}
+                </ul>
+            </body>
+            </html>`
+}
+
+function renderEmployee(employee) {
+    var specialDatum = ''
+
+    switch (employee.getRole()) {
+        case 'Manager':
+            specialDatum = `Office number: ${employee.getOfficeNumber()}`
+            break
+        case 'Engineer':
+            specialDatum = `GitHub: <a href="${employee.getGithub()}" target="_blank">${employee.getGithub()}</a>`
+            break
+        case 'Intern':
+            specialDatum = `School: ${employee.getSchool()}`
+            break
+    }
+
+    return `<li>
+                <h2>${employee.getName()}</h2>
+                <h3>${employee.getRole()}</h3>
+                <p>ID: ${employee.getId()}</p>
+                <p>Email: <a href="mailto:${employee.getEmail()}">${employee.getEmail()}</a></p>
+                <p>${specialDatum}</p>
+            </li>`
+}
+
+module.exports = renderTeamRoster
